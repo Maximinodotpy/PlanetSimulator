c9451fc07609b96578e6fe772734c5b8b939012a
Maxim Maeder <85794303+Maximinodotpy@users.noreply.github.com>
2023-10-18 15:45:39 +0200

    Added a Placeholder for when there are no planets or when there is no selection

--- Scenes/User Interface/Editor/Objects.gd
+++ Scenes/User Interface/Editor/Objects.gd
@@ -1,6 +1,7 @@
 extends VBoxContainer
 
 var objectRowScene = preload("res://Scenes/User Interface/Editor/Parts/Object Row.tscn")
+@onready var no_objects = $CenterContainer/no_objects
 
 func _ready():
 	Objects.on_object_added.connect(create_object_row)
@@ -13,3 +14,6 @@ func create_object_row(node: Node2D):
 	object_row.object = node
 
 	add_child(object_row)
+
+func _process(delta):
+	no_objects.visible = not Objects.is_there_objects()
